72f7417a7acf4393cef369264017ea413be8491f
Kevin Cai <kevincai96@gmail.com>
2015-07-15 20:52:30 -0400

    Coded recovery from brief network disconnect

--- src/syn/threads/NetworkThread.java
+++ src/syn/threads/NetworkThread.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.SocketException;
 
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
@@ -14,7 +15,7 @@ import syn.utils.Settings;
 import syn.utils.Utilities;
 
 public class NetworkThread implements Runnable {
-	
+
 	private String channel;
 	//private Socket socket;
 	private SSLSocket socket;
@@ -23,7 +24,7 @@ public class NetworkThread implements Runnable {
 	private Utilities uc;
 	private String line;
 	private String nick;
-	
+
 	public NetworkThread(String server, String channel, int port) {
 		try {
 			this.channel = channel;
@@ -38,49 +39,73 @@ public class NetworkThread implements Runnable {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void run() {
-		try {
-			connect();
-		} catch (IOException e) {e.printStackTrace();}
+		if(connect()) {
+			readLines();
+		}
 	}
-	
-	private void connect() throws IOException {	
-		nick = uc.procOS(Client.getInstance().os) + "-" + uc.genCode();
-		String login = "synth";
 
-		// Log on to the server.
-		writer.write("NICK " + nick + "\r\n");
-		writer.write("USER " + login + " 8 * : Synthetic\r\n");
-		writer.flush();
+	private boolean connect() {	
+		try {
+			nick = uc.procOS(Client.getInstance().os) + "-" + uc.genCode();
+			String login = "synth";
 
-		// Read lines from the server until it tells us we have connected.
-		while((line = reader.readLine()) != null) {
-			Client.getInstance().cmdThread.setLine(line);
-			if(line.contains("004")) {
-				// We are now logged in.
-				break;
-			} else if (line.indexOf("433") >= 0) {
-				//Retry connection with diff nick
-				return;
+			// Log on to the server.
+			writer.write("NICK " + nick + "\r\n");
+			writer.write("USER " + login + " 8 * : Synthetic\r\n");
+			writer.flush();
+
+			// Read lines from the server until it tells us we have connected.
+			while((line = reader.readLine()) != null) {
+				Client.getInstance().cmdThread.setLine(line);
+				if(line.contains("004")) {
+					// We are now logged in.
+					break;
+				} else if (line.indexOf("433") >= 0) {
+					//Retry connection with diff nick
+					return false;
+				}
+			}
+
+			// Join the channel.
+			writer.write("JOIN " + channel + " " + Settings.channelPass + "\r\n");
+			writer.flush();
+			Client.getInstance().connected = true;
+			return true;
+		} catch (Exception e) {}
+		return false;
+	}
+
+	private void readLines() {
+		// Keep reading lines from the server.
+		try {
+			while((line = reader.readLine()) != null) {
+				Client.getInstance().cmdThread.setLine(line);
+				// Print the raw line received by the bot.
+				//TODO: DEBUG 
+			}
+		} catch (Exception e) {
+			//Disconnected, reconnect if it's recv failed
+			if(e instanceof SocketException && Client.getInstance().connected) {
+				while(true) {
+					try {
+						Client.getInstance().connected = false;
+						connect();
+						if(Client.getInstance().connected) {
+							break;
+						} else {
+							Thread.sleep(2000);
+						}
+					} catch (Exception e1) {
+						e1.printStackTrace();
+					}
+				}
+				readLines();
 			}
 		}
-
-		// Join the channel.
-		writer.write("JOIN " + channel + " " + Settings.channelPass + "\r\n");
-		writer.flush();
-		Client.getInstance().connected = true;
-
-		// Keep reading lines from the server.
-		while((line = reader.readLine()) != null) {
-			Client.getInstance().cmdThread.setLine(line);
-			// Print the raw line received by the bot.
-			//TODO: DEBUG 
-		}
-		
-		//Disconnected. Auto reconnect if DC is accidental
 	}
-	
+
 	public void disconnect() {
 		try {
 			Client.getInstance().dupePrevent.close();
@@ -88,11 +113,11 @@ public class NetworkThread implements Runnable {
 			System.exit(0);
 		} catch (IOException e) {}
 	}
-	
+
 	public String getChannel() {
 		return channel;
 	}
-	
+
 	public BufferedWriter getWriter() {
 		return writer;
 	}
